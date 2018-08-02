634ad796db980f1202206cc6a862058f43806204
Alex Widner <manny1806@gmail.com>
2018-08-02 13:11:04 -0700

    book sort

---
+++ book-sort.js
@@ -0,0 +1,13 @@
+//The quick sort approach seems similar to how books are sorted in actual libraries. If we were to covert the book
+//titles to lower case and examine their character codes we would be able to use a quick sort algorithm to sort them
+//alphabetically 
+
+// step 1 find a book close to beginning or end of alphabet
+
+// step 2 find other books that start with the same letter
+
+// step 3 move second book to the correct side of first book
+
+// step 4 continue to find books that start with the same letter and place them between the already placed books, in order
+
+// step 5 repeat with other letters
